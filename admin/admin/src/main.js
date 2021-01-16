84ef7415c33f5ac51da77ec6c71626b7bafa375c
yang <1327462182@qq.com>
2021-01-16 12:53:58 +0800

    init element-ui

--- admin/admin/src/main.js
+++ admin/admin/src/main.js
@@ -1,9 +1,15 @@
-import Vue, { createApp } from 'vue';
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+//引入element-ui组件
+import './plugins'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
